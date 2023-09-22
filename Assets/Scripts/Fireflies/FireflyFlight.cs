1d4c0c7998d8ced6e0cbd9530e49501c796e48ab
A. Roig <antiaroig@gmail.com>
2023-09-23 01:25:04 +0200

    asdas

--- Assets/Scripts/Fireflies/FireflyFlight.cs
+++ Assets/Scripts/Fireflies/FireflyFlight.cs
@@ -6,7 +6,8 @@ public class FireflyFlight : MonoBehaviour
 {
     [SerializeField] float speed = 1.0f;
     [SerializeField] float randomMovementRange = 0.5f;
-    [SerializeField] private List<Transform> positions; 
+    [SerializeField] private List<Transform> positions;
+    [SerializeField] private LayerMask platformLayer;
 
     private Rigidbody2D _rb;
     private Vector3 _newPosition;
@@ -50,8 +51,40 @@ public class FireflyFlight : MonoBehaviour
             var position = transform.position;
             position = new Vector3(position.x + (Random.Range(-randomMovementRange, randomMovementRange)),
                 position.y + (Random.Range(-randomMovementRange, randomMovementRange)));
+            RaycastHit2D raycastHit2D = Physics2D.Linecast(transform.position, position, platformLayer);
+
+            if (raycastHit2D)
+            {
+                Debug.Log(raycastHit2D.distance);
+                Debug.Log(raycastHit2D.point + " - " + Vector2.ClampMagnitude(raycastHit2D.point, raycastHit2D.distance));
+                position = raycastHit2D.point - Vector2.ClampMagnitude(raycastHit2D.point, raycastHit2D.distance);
+            }
+                
             transform.position = position;
+            
             yield return new WaitForSeconds(Random.Range(0.2f, 0.3f));
         }
+        
+        /*
+                 while (true)
+           {
+           bool shouldRepeat;
+           Vector3 position;
+           
+           do
+           {
+           position = transform.position;
+           position = new Vector3(position.x + (Random.Range(-randomMovementRange, randomMovementRange)),
+           position.y + (Random.Range(-randomMovementRange, randomMovementRange)));
+           
+           shouldRepeat = Physics2D.OverlapBox(position, new Vector2(1, 1), 0, platformLayer);
+           
+           } while (shouldRepeat);
+           
+           transform.position = position;
+           yield return new WaitForSeconds(Random.Range(0.2f, 0.3f));
+           }
+         
+         */
     }
 }
